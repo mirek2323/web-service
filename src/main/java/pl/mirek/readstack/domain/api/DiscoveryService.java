6b7d059ed21e0feef758c912dffd2bc79f762523
mirek2323 <60038040+mirek2323@users.noreply.github.com>
2022-03-20 18:46:02 +0100

    Add files via upload

---
+++ src/main/java/pl/mirek/readstack/domain/api/DiscoveryService.java
@@ -0,0 +1,56 @@
+package pl.mirek.readstack.domain.api;
+
+import pl.mirek.readstack.domain.discovery.Discovery;
+import pl.mirek.readstack.domain.discovery.DiscoveryDao;
+import pl.mirek.readstack.domain.user.UserDao;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DiscoveryService {
+    private final DiscoveryDao discoveryDao = new DiscoveryDao();
+    private final DiscoveryMapper discoveryMapper = new DiscoveryMapper();
+
+    public void add(DiscoverySaveRequest saveRequest) {
+        Discovery discoveryToSave = discoveryMapper.map(saveRequest);
+        discoveryDao.save(discoveryToSave);
+    }
+
+    public List<DiscoveryBasicInfo> findAll() {
+        return discoveryDao.findAll()
+                .stream().map(discoveryMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    public List<DiscoveryBasicInfo> findAllByCategory(int categoryId) {
+        return discoveryDao.findByCategory(categoryId)
+                .stream().map(discoveryMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    private static class DiscoveryMapper {
+        private final UserDao userDao = new UserDao();
+
+        DiscoveryBasicInfo map(Discovery d) {
+            return new DiscoveryBasicInfo(
+                    d.getTitle(),
+                    d.getUrl(),
+                    d.getDescription(),
+                    d.getDateAdded()
+            );
+        }
+
+        Discovery map(DiscoverySaveRequest ds) {
+            return new Discovery(
+                    ds.getTitle(),
+                    ds.getUrl(),
+                    ds.getDescription(),
+                    LocalDateTime.now(),
+                    ds.getCategoryId(),
+                    userDao.findByUsername(ds.getAuthor())
+                            .orElseThrow()
+                            .getId()
+            );
+        }
+    }
+}
